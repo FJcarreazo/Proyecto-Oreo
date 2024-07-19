3a778a75bd306c518484eb6e1af678bca237b75b
Erick Hernandez <takashi.onimaru@gmail.com>
2024-07-19 18:36:19 -0400

    "Refactor code in views/inicio/index.js: reformatted code, removed unnecessary whitespace, and added/removed some line breaks."

--- views/inicio/index.js
+++ views/inicio/index.js
@@ -12,48 +12,48 @@ const courseBtn = document.querySelectorAll("#course-btn");
 
 
 shopIcon.addEventListener('click', e => {
-    cart.classList.add('h-96', 'p-4');
-    
+  cart.classList.add('h-96', 'p-4');
+
 });
 
 closeCart.addEventListener('click', e => {
-    cart.classList.remove('h-96', 'p-4');
-    
-}); 
+  cart.classList.remove('h-96', 'p-4');
 
-sessionBtn.addEventListener('click', e => {
-    seccion.classList.add('h-96', 'p-4');
-    
-}); 
-
-vovlerBtn.addEventListener('click', e => {
-    seccion.classList.remove('h-96', 'p-4');
-    
-}); 
-
-cerrarSeccion.addEventListener('click', async e => {
-    try {
-        await axios.get('/api/logout');
-        window.location.pathname = '/';
-    } catch (error) {
-        console.log(error);
-    }
 });
 
-courseBtn.forEach(btn =>{
+sessionBtn.addEventListener('click', e => {
+  seccion.classList.add('h-96', 'p-4');
+
+});
+
+vovlerBtn.addEventListener('click', e => {
+  seccion.classList.remove('h-96', 'p-4');
+
+});
+
+cerrarSeccion.addEventListener('click', async e => {
+  try {
+    await axios.get('/api/logout');
+    window.location.pathname = '/';
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+courseBtn.forEach(btn => {
   btn.addEventListener('click', e => {
     console.log(e.target);
-      const img = e.target.parentElement.parentElement.children[0].innerHTML;
-      const name = e.target.parentElement.children[0].innerHTML;
+    const img = e.target.parentElement.parentElement.children[0].innerHTML;
+    const name = e.target.parentElement.children[0].innerHTML;
 
 
-      const exist = [... table.children].find(Element => Element.children[1].innerHTML == name);
-      if (exist) {
-          exist.children[3].innerHTML = Number(exist.children[3].innerHTML) +1;
-      } else {
-          const row = document.createElement('tr');
+    const exist = [...table.children].find(Element => Element.children[1].innerHTML == name);
+    if (exist) {
+      exist.children[3].innerHTML = Number(exist.children[3].innerHTML) + 1;
+    } else {
+      const row = document.createElement('tr');
 
-          row.innerHTML = `
+      row.innerHTML = `
           <td>${img}</td>
           <td>${name}</td>
           <td>15$</td>
@@ -65,54 +65,53 @@ courseBtn.forEach(btn =>{
           </td>
           `;
 
-          row.children[4].addEventListener('click', e => {
-              e.currentTarget.parentElement.remove();
-          }); 
-
-
-  
-          table.append(row);
-  
-      }
+      row.children[4].addEventListener('click', e => {
+        e.currentTarget.parentElement.remove();
+      });
 
 
 
-     
+      table.append(row);
+
+    }
+
+
+
+
   });
 });
 
- // Función para obtener productos desde el backend
- async function fetchProducts() {
-    try {
-      const response = await axios.get('http://localhost:3003/api/products');
-      const products = response.data;
-     
-      
+// Función para obtener productos desde el backend
+async function fetchProducts() {
+  try {
+    const response = await axios.get('http://localhost:3003/api/products');
+    const products = response.data;
 
-      // Renderizar la lista de productos
-      products.forEach(product => {
-        const div = document.createElement('div');
-        div.innerHTML = `
+
+
+    // Renderizar la lista de productos
+    products.forEach(product => {
+      const div = document.createElement('div');
+      div.innerHTML = `
         <div class="gap-1 flex flex-col items-center justify-center">
          <img class="h-40 w-40 rounded-3xl" src="${product.image}"/> 
-          <h3 class="font-['Pally'] font-bold text-base">Nombre: ${product.name}</h3>
-          <p class="font-['Pally'] font-bold text-base">Descripcion: ${product.description}</p>
-          <p class="font-['Pally'] font-bold text-base">Precio: $${product.price}</p>
-          <p class="font-['Pally'] font-bold text-base">Cantidad: ${product.quantity}</p>
+          <h3 class="font-['Pally'] font-bold text-base">${product.name}</h3>
+          <p class="font-['Pally']  text-base">${product.description}</p>
+          <p class="font-['Pally'] font-bold text-base">$${product.price}</p>
           <button id="course-btn" class="h-8 w-40 rounded-xl bg-[#485935] hover:bg-[#26301b] font-['Pally'] text-white">Agreagar al carrito</button>
           </div>
         `;
-        productList.appendChild(div);
-      });
-    } catch (error) {
-      console.error('Error al obtener productos:', error);
-    }
+      productList.appendChild(div);
+    });
+  } catch (error) {
+    console.error('Error al obtener productos:', error);
   }
+}
 
-  // Llamar a la función para obtener productos al cargar la página
-  fetchProducts();
+// Llamar a la función para obtener productos al cargar la página
+fetchProducts();
 
- 
+
 
 
 
