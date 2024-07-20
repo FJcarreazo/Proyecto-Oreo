475a3b45b1ec4476b168df2eb6fb64f3e8fb4183
Erick Hernandez <takashi.onimaru@gmail.com>
2024-07-19 20:13:14 -0400

    Carrito funcionando

--- views/inicio/index.js
+++ views/inicio/index.js
@@ -1,53 +1,52 @@
-const shopIcon = document.querySelector('#shop-icon');
-const cart = document.querySelector('#cart');
-const table = document.querySelector('#table-body');
-const tableClear = document.querySelector('#table-clear');
-const closeCart = document.querySelector('#close-cart');
-const seccion = document.querySelector('#seccion');
-const sessionBtn = document.querySelector('#cerrar-btn');
-const vovlerBtn = document.querySelector('#volver');
-const cerrarSeccion = document.querySelector('#sesion-btn')
-const productList = document.querySelector("#product-list");
-const courseBtn = document.querySelectorAll("#course-btn");
+$(document).ready(function () {
+  const cart = document.querySelector('#cart');
+  const table = document.querySelector('#table-body');
+  const closeCart = document.querySelector('#close-cart');
+  const seccion = document.querySelector('#seccion');
+  const sessionBtn = document.querySelector('#cerrar-btn');
+  const vovlerBtn = document.querySelector('#volver');
+  const cerrarSeccion = document.querySelector('#sesion-btn')
+  const productList = document.querySelector("#product-list");
+  const courseBtn = document.querySelectorAll("#course-btn");
 
-shopIcon.addEventListener('click', e => {
-  cart.classList.add('h-96', 'p-4');
-});
+  $("#shop-icon").on('click', function () {
+    $("#cart").toggleClass("h-96 p-4");
+  });
 
-closeCart.addEventListener('click', e => {
-  cart.classList.remove('h-96', 'p-4');
-});
+  closeCart.addEventListener('click', e => {
+    cart.classList.remove('h-96', 'p-4');
+  });
 
-sessionBtn.addEventListener('click', e => {
-  seccion.classList.add('h-96', 'p-4');
-});
+  sessionBtn.addEventListener('click', e => {
+    seccion.classList.add('h-96', 'p-4');
+  });
 
-vovlerBtn.addEventListener('click', e => {
-  seccion.classList.remove('h-96', 'p-4');
-});
+  vovlerBtn.addEventListener('click', e => {
+    seccion.classList.remove('h-96', 'p-4');
+  });
 
-cerrarSeccion.addEventListener('click', async e => {
-  try {
-    await axios.get('/api/logout');
-    window.location.pathname = '/';
-  } catch (error) {
-    console.log(error);
-  }
-});
+  cerrarSeccion.addEventListener('click', async e => {
+    try {
+      await axios.get('/api/logout');
+      window.location.pathname = '/';
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
-courseBtn.forEach(btn => {
-  btn.addEventListener('click', e => {
-    console.log(e.target);
-    const img = e.target.parentElement.parentElement.children[0].innerHTML;
-    const name = e.target.parentElement.children[0].innerHTML;
-    const exist = [...table.children].find(Element => Element.children[1].innerHTML == name);
+  courseBtn.forEach(btn => {
+    btn.addEventListener('click', e => {
+      console.log(e.target);
+      const img = e.target.parentElement.parentElement.children[0].innerHTML;
+      const name = e.target.parentElement.children[0].innerHTML;
+      const exist = [...table.children].find(Element => Element.children[1].innerHTML == name);
 
-    if (exist) {
-      exist.children[3].innerHTML = Number(exist.children[3].innerHTML) + 1;
-    } else {
-      const row = document.createElement('tr');
+      if (exist) {
+        exist.children[3].innerHTML = Number(exist.children[3].innerHTML) + 1;
+      } else {
+        const row = document.createElement('tr');
 
-      row.innerHTML = `
+        row.innerHTML = `
           <td>${img}</td>
           <td>${name}</td>
           <td>15$</td>
@@ -58,40 +57,114 @@ courseBtn.forEach(btn => {
             </svg>
           </td>
           `;
-      row.children[4].addEventListener('click', e => {
-        e.currentTarget.parentElement.remove();
-      });
+        row.children[4].addEventListener('click', e => {
+          e.currentTarget.parentElement.remove();
+        });
 
-      table.append(row);
-    }
+        table.append(row);
+      }
 
+    });
   });
-});
 
-// Función para obtener productos desde el backend
-async function fetchProducts() {
-  try {
-    const response = await axios.get('/api/products');
-    const products = response.data;
+  // Función para obtener productos desde el backend
+  async function fetchProducts() {
+    try {
+      const response = await axios.get('/api/products');
+      const products = response.data;
 
-    // Renderizar la lista de productos
-    products.forEach(product => {
-      const div = document.createElement('div');
-      div.innerHTML = `
-        <div class="gap-1 flex flex-col items-center justify-center">
-         <img class="h-40 w-40 rounded-3xl" src="${product.image}"/> 
+      // Renderizar la lista de productos
+      products.forEach(product => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div 
+          class="gap-1 flex flex-col items-center justify-center productItem" 
+          id="${product._id}"
+          data-name="${product.name}"
+          data-description="${product.description}"
+          data-price="${product.price}"
+        >
+          <img class="h-40 w-40 rounded-3xl" src="${product.image}"/> 
           <h3 class="font-['Pally'] font-bold text-base">${product.name}</h3>
           <p class="font-['Pally']  text-base">${product.description}</p>
           <p class="font-['Pally'] font-bold text-base">$${product.price}</p>
-          <button id="course-btn" class="h-8 w-40 rounded-xl bg-[#485935] hover:bg-[#26301b] font-['Pally'] text-white">Agreagar al carrito</button>
-          </div>
+          <button class="h-8 w-40 rounded-xl bg-[#485935] hover:bg-[#26301b] font-['Pally'] text-white course-btn">
+            Agregar al carrito
+          </button>
+        </div>
         `;
-      productList.appendChild(div);
-    });
-  } catch (error) {
-    console.error('Error al obtener productos:', error);
+        productList.appendChild(div);
+      });
+    } catch (error) {
+      console.error('Error al obtener productos:', error);
+    }
   }
-}
 
-// Llamar a la función para obtener productos al cargar la página
-fetchProducts();
+  //CLICK EDIT BUTTON
+  $('body').on('click', '.course-btn', function (e) {
+    e.preventDefault();
+    const item = $(this).parent();
+    const id = item.attr('id');
+    const name = item.data('name');
+    const description = item.data('description');
+    const price = item.data('price');
+    let bandera = false;
+    let itemCarrito = "";
+    if (localStorage.carrito) {
+      itemCarrito = JSON.parse(localStorage.carrito);
+      const carritoDuplicado = itemCarrito.find(item => item.id === id);
+      if (carritoDuplicado) {
+        bootbox.alert('No puede comprar mas de un articulo');
+      } else {
+        itemCarrito.push({
+          id,
+          name,
+          description,
+          price
+        });
+        bandera = true;
+      }
+
+    } else {
+      itemCarrito = [
+        {
+          id,
+          name,
+          description,
+          price
+        }
+      ];
+      bandera = true;
+    }
+    localStorage.setItem('carrito', JSON.stringify(itemCarrito));
+    if (bandera) {
+      showCart();
+      $('#cart').addClass('h-96 p-4');
+    }
+
+  })
+
+  const showCart = () => {
+    $('#table-body-cart').html('');
+    if (localStorage.carrito) {
+      const carrito = JSON.parse(localStorage.carrito);
+      carrito.map(item => {
+        $('#table-body-cart').append(`
+        <tr>
+          <td class="font-bold  borber-b">${item.name}</td>
+          <td class="borber-b">${item.description}</td>
+          <td class="font-bold borber-b">${item.price}</td>
+        </tr>
+        `);
+      })
+    }
+  }
+
+  $('#table-clear').on('click', function () {
+    localStorage.removeItem('carrito');
+    showCart();
+  });
+
+  // Llamar a la función para obtener productos al cargar la página
+  fetchProducts();
+});
